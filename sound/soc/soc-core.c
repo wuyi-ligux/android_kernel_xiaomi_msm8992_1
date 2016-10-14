4f7f16c87666e4f0fff91f1ff6ab8286a2953eca
Yuan Lin <yualin@google.com>
2016-10-14 16:38:09 -0700

    ASoC: check for null function pointer for dummy device read/write, take 2
    Change null function pointer's error return code from -EIO to -1
    because snd_soc_read and snd_soc_write both have unsigned int as
    return type.
    
    Bug: 28838221
    Change-Id: I0667f1f35fa1c23349eb6dcd6cabff616120be86
    Signed-off-by: Yuan Lin <yualin@google.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -2107,9 +2107,10 @@ unsigned int snd_soc_read(struct snd_soc_codec *codec, unsigned int reg)
 		ret = codec->read(codec, reg);
 		dev_dbg(codec->dev, "read %x => %x\n", reg, ret);
 		trace_snd_soc_reg_read(codec, reg, ret);
-	} else {
-		ret = -EIO;
 	}
+	else
+		ret = -1;
+
 	return ret;
 }
 EXPORT_SYMBOL_GPL(snd_soc_read);
@@ -2121,9 +2122,9 @@ unsigned int snd_soc_write(struct snd_soc_codec *codec,
 		dev_dbg(codec->dev, "write %x = %x\n", reg, val);
 		trace_snd_soc_reg_write(codec, reg, val);
 		return codec->write(codec, reg, val);
-	} else {
-		return -EIO;
 	}
+	else
+		return -1;
 }
 EXPORT_SYMBOL_GPL(snd_soc_write);
 
